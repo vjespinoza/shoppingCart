ba88ea56f87ccac24f94ef66e445e07756460ab6
vjespinoza <vjespinoza84@gmail.com>
2020-09-24 17:32:00 -0500

    design first draft

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//First test
+//Store information inside an object
+
+const productList = {
+  itemImage: document.querySelectorAll(".itemImage"),
+  itemName: document.querySelectorAll(".itemName"),
+  itemModel: document.querySelectorAll(".itemModel"),
+  itemPrice: document.querySelectorAll(".itemPrice"),
+};
+
+console.log(productList.itemImage);
+console.log(productList.itemName);
+console.log(productList.itemModel);
+console.log(productList.itemPrice);
