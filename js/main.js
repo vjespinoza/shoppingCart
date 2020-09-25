0cff49bd0f6c882ce4e0a2707c8ea9a5316ce44b
vjespinoza <vjespinoza84@gmail.com>
2020-09-25 12:21:18 -0500

    fail!

--- js/main.js
+++ js/main.js
@@ -1,24 +1,40 @@
 //First test
 //Store information inside an object
 
-let itemImage = document.querySelectorAll(".itemImage");
-let itemName = document.querySelectorAll(".itemName");
-let itemModel = document.querySelectorAll(".itemModel");
-let itemPrice = document.querySelectorAll(".itemPrice");
-let addToCartBtn = document.querySelectorAll(".addToCartBtn");
-let money = document.getElementById("cartMoney");
-
-addToCartBtn.forEach((addCartBtn) => {
-  addCartBtn.addEventListener("click", () => {
-    itemPrice.forEach((price) => {
-      money.innerHTML = `fucking fuck ${price.innerHTML}`;
-      console.log(price.innerHTML);
+const shopItems = {
+  itemImage: document.querySelectorAll(".itemImage"),
+  itemName: document.querySelectorAll(".itemName"),
+  itemModel: document.querySelectorAll(".itemModel"),
+  itemPrice: document.querySelectorAll(".itemPrice"),
+  addToCartBtn: document.querySelectorAll(".addToCartBtn"),
+  money: document.getElementById("cartMoney"),
+  getIdPairs: function () {
+    let a = shopItems.itemPrice.forEach((price) => {
+      var i = price.getAttribute("id");
+      i.slice(i.length - 2);
     });
-    this.price;
+
+    let b = shopItems.addToCartBtn.forEach((btn) => {
+      var i = btn.getAttribute("id");
+      i.slice(i.length - 2);
+    });
+
+    // let c = [a * 2] - b;
+    // let c = 2;
+
+    if (a == b) {
+      return (shopItems.money.innerHTML = `${c}`);
+    }
+  },
+};
+
+shopItems.addToCartBtn.forEach((addCartBtn) => {
+  addCartBtn.addEventListener("click", () => {
+    shopItems.getIdPairs();
   });
 });
 
-// console.log(itemImage);
+// console.log(shopItems.);
 // console.log(itemName);
 // console.log(itemModel);
 // console.log(itemPrice);
