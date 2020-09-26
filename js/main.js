a52b617608400f3e954c4319e98ab898736271bd
vjespinoza <vjespinoza84@gmail.com>
2020-09-25 19:13:21 -0500

    price added to priceDisplay on abbToCartButton click

--- js/main.js
+++ js/main.js
@@ -1,54 +1,25 @@
 //First test
 //Store information inside an object
 
-const shopItems = {
-  itemImage: document.querySelectorAll(".itemImage"),
-  itemName: document.querySelectorAll(".itemName"),
-  itemModel: document.querySelectorAll(".itemModel"),
-  itemPrice: document.querySelectorAll(".itemPrice"),
-  addToCartBtn: document.querySelectorAll(".addToCartBtn"),
-  money: document.getElementById("cartMoney"),
-};
+let itemImage = document.querySelectorAll(".itemImage");
+let itemName = document.querySelectorAll(".itemName");
+let itemModel = document.querySelectorAll(".itemModel");
+let itemPrice = document.querySelectorAll(".itemPrice");
+let addToCartBtn = document.querySelectorAll(".addToCartBtn");
+let money = document.getElementById("cartMoney");
 
 function getButtonId() {
-  shopItems.addToCartBtn.forEach((addCartBtn) => {
+  addToCartBtn.forEach((addCartBtn) => {
     addCartBtn.addEventListener("click", (e) => {
-      //Gets addToCartBtn button ID's last two characters
       var btnId = e.target.getAttribute("id");
-      btnId = btnId.slice(btnId.length - 2);
-      // console.log(btnId);
-      return btnId;
+      btnId = btnId.slice(btnId.length - 2) - 1;
+      var x = itemPrice.item(btnId).id.slice(length - 2) - 1;
+      var z = parseFloat(itemPrice.item(btnId).innerHTML.slice(1));
+      if (btnId == x) {
+        money.innerHTML = `$${z}`;
+      }
     });
   });
 }
 
-function getPriceId() {
-  shopItems.itemPrice.forEach((itemPriceTag) => {
-    itemPriceTag.addEventListener("click", (e) => {
-      var priceId = e.target.getAttribute("id");
-      priceId = priceId.slice(priceId.length - 2);
-      // console.log(priceId);
-      return priceId;
-    });
-  });
-}
-
-function testTest() {
-  let a = getButtonId();
-  let b = getPriceId();
-  console.log(a + b);
-}
-
-testTest();
-
-// console.log(shopItems.);
-// console.log(itemName);
-// console.log(itemModel);
-// console.log(itemPrice);
-// console.log(addToCartBtn);
-
-//Gets last 2 digits of the elemt's ID
-// shopItems.itemPrice.forEach((price) => {
-//   var i = price.getAttribute("id");
-//   console.log(i.slice(i.length - 2));
-// });
+getButtonId();
