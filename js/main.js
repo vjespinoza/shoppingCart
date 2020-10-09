1b4d998461ceee1dedc37790197ea65e989320bc
vjespinoza <vjespinoza84@gmail.com>
2020-10-08 19:41:53 -0500

    finally able to get summaryItems on the cart details page

--- js/main.js
+++ js/main.js
@@ -26,124 +26,128 @@ let modalCancelBtn = document.getElementById("modalCancelBtn");
 
 //Click event listener for .addToCartBtn
 addToCartBtn.forEach((addCartBtn) => {
-  addCartBtn.addEventListener("click", (e) => {
-    //PENDING!!! Click event to prevent pointer events when modal is active
+    addCartBtn.addEventListener("click", (e) => {
+        //PENDING!!! Click event to prevent pointer events when modal is active
 
-    function updateModal() {
-      modal.style.display = "block";
+        function updateModal() {
+            modal.style.display = "block";
 
-      //Gets addToCartBtn ID as string and slice last two characters
-      var btnId = e.target.getAttribute("id");
-      btnId = btnId.slice(btnId.length - 2) - 1;
-      //btnId defines the item position for itemPrice, then gets ID
-      //as string and slice last two characters
-      var x = itemPrice.item(btnId).id.slice(length - 2) - 1;
-      //Gets innerHTML content ox x, and slices the first character ($).
-      var z = parseFloat(itemPrice.item(btnId).innerText);
+            //Gets addToCartBtn ID as string and slice last two characters
+            var btnId = e.target.getAttribute("id");
+            btnId = btnId.slice(btnId.length - 2) - 1;
+            //btnId defines the item position for itemPrice, then gets ID
+            //as string and slice last two characters
+            var x = itemPrice.item(btnId).id.slice(length - 2) - 1;
+            //Gets innerHTML content ox x, and slices the first character ($).
+            var z = parseFloat(itemPrice.item(btnId).innerText);
 
-      var img = itemImage.item(btnId).getAttribute("src");
+            var img = itemImage.item(btnId).getAttribute("src");
 
-      var name = itemName.item(btnId).innerText;
-      var model = itemModel.item(btnId).innerText;
+            var name = itemName.item(btnId).innerText;
+            var model = itemModel.item(btnId).innerText;
 
-      if (btnId == x) {
-        displayModalTotal.innerText = `${z}`;
-        displayModalPrice.innerText = `${z}`;
-        displayModalQty.value = 1;
-        displayModalName.innerText = name.toUpperCase();
-        displayModalModel.innerText = model.toUpperCase();
-        displayModalImage.innerHTML = `<img src="${img}" />`;
-      }
-    }
+            if (btnId == x) {
+                displayModalTotal.innerText = `${z}`;
+                displayModalPrice.innerText = `${z}`;
+                displayModalQty.value = 1;
+                displayModalName.innerText = name.toUpperCase();
+                displayModalModel.innerText = model.toUpperCase();
+                displayModalImage.innerHTML = `<img src="${img}" />`;
+            }
+        }
 
-    // updatePriceDisplay();
-    updateModal();
-  });
+        // updatePriceDisplay();
+        updateModal();
+    });
 });
 
 modalBtnAdd.addEventListener("click", () => {
-  var a = parseInt(displayModalQty.value);
-  displayModalQty.value = ++a;
-  var b = parseFloat(displayModalPrice.innerText);
-  var total = a * b;
+    var a = parseInt(displayModalQty.value);
+    displayModalQty.value = ++a;
+    var b = parseFloat(displayModalPrice.innerText);
+    var total = a * b;
 
-  displayModalTotal.innerText = `${total.toFixed(2)}`;
+    displayModalTotal.innerText = `${total.toFixed(2)}`;
 });
 
 modalBtnRemove.addEventListener("click", () => {
-  var a = parseInt(displayModalQty.value);
-  displayModalQty.value = --a;
-  var b = parseFloat(displayModalPrice.innerText);
-  var total = a * b;
+    var a = parseInt(displayModalQty.value);
+    displayModalQty.value = --a;
+    var b = parseFloat(displayModalPrice.innerText);
+    var total = a * b;
 
-  if (total <= 0) {
-    displayModalQty.value = 1;
-  } else {
-    displayModalTotal.innerText = `${total.toFixed(2)}`;
-  }
+    if (total <= 0) {
+        displayModalQty.value = 1;
+    } else {
+        displayModalTotal.innerText = `${total.toFixed(2)}`;
+    }
 });
 
 let itemList = [];
 
 modalCofirmBtn.addEventListener("click", () => {
-  modal.style.display = "none";
+    modal.style.display = "none";
 
-  let x = parseFloat(displayModalTotal.innerText);
-  let y = x.toLocaleString("es-US", {
-    style: "decimal",
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  });
+    let x = parseFloat(displayModalTotal.innerText);
+    let y = x.toLocaleString("es-US", {
+        style: "decimal",
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+    });
 
-  //Stores displayModalTotal values into an array | let displayMoneyTotal = [];
-  displayMoneyTotal.push(y);
+    //Stores displayModalTotal values into an array | let displayMoneyTotal = [];
+    displayMoneyTotal.push(y);
 
-  //Sums all values of the array using reduce()
-  //and displays the result on displayMoney.innerText
-  displayMoney.innerText = `$${displayMoneyTotal.reduce(addTotals)}`;
+    //Sums all values of the array using reduce()
+    //and displays the result on displayMoney.innerText
+    displayMoney.innerText = `$${displayMoneyTotal.reduce(addTotals)}`;
 
-  //Function required to proerly use reduce()
-  function addTotals(total, newTotal) {
-    return (parseFloat(total) + parseFloat(newTotal)).toFixed(2);
-  }
+    //Function required to proerly use reduce()
+    function addTotals(total, newTotal) {
+        return (parseFloat(total) + parseFloat(newTotal)).toFixed(2);
+    }
 
-  //Same as above for the number of items on the cart.
-  displayItemsTotal.push(displayModalQty.value);
+    //Same as above for the number of items on the cart.
+    displayItemsTotal.push(displayModalQty.value);
 
-  displayItems.innerText = `(Items: ${displayItemsTotal.reduce(itemsTotal)})`;
+    displayItems.innerText = `(Items: ${displayItemsTotal.reduce(itemsTotal)})`;
 
-  function itemsTotal(item, newItem) {
-    return parseInt(item) + parseInt(newItem);
-  }
+    function itemsTotal(item, newItem) {
+        return parseInt(item) + parseInt(newItem);
+    }
 
-  //Set localStorge for modal info
+    //Set localStorge for modal info
 
-  let modalItem = {
-    modalItemImage: displayModalImage.innerHTML.slice(10, -2),
-    modalItemName: displayModalName.innerText,
-    modalItemModel: displayModalModel.innerText,
-    modalItemQuantity: displayModalQty.value,
-    modalItemUnitPrice: displayModalPrice.innerText,
-    modalItemTotal: displayModalTotal.innerText,
-  };
+    let modalItem = {
+        modalItemImage: displayModalImage.innerHTML.slice(10, -2),
+        modalItemName: displayModalName.innerText,
+        modalItemModel: displayModalModel.innerText,
+        modalItemQuantity: displayModalQty.value,
+        modalItemUnitPrice: displayModalPrice.innerText,
+        modalItemTotal: displayModalTotal.innerText,
+    };
 
-  itemList.push(modalItem);
-  localStorage.setItem("orderItem", JSON.stringify(itemList));
+    itemList.push(modalItem);
+    localStorage.setItem("orderItem", JSON.stringify(itemList));
 
-  console.log(itemList); //Array
-  console.log(localStorage);
-  console.log(localStorage.orderItem);
+    console.log(itemList); //Array
+    console.log(localStorage);
+    console.log(localStorage.orderItem);
 
-  //PENDING!!! Validates if an item has already been added to the cart.
+    //PENDING!!! Validates if an item has already been added to the cart.
 });
 
 modalCancelBtn.addEventListener("click", () => {
-  modal.style.display = "none";
+    modal.style.display = "none";
 });
 
 //PROVISIONAL FUNCTION
 function x() {
-  localStorage.clear();
-  console.log(localStorage);
-  location.reload();
+    localStorage.clear();
+    console.log(localStorage);
+    location.reload();
 }
+
+window.addEventListener("unload", () => {
+    localStorage.clear();
+});
--- js/second.js
+++ js/second.js
@@ -10,65 +10,39 @@ let summaryModel = document.getElementById("itemModel_01");
 let summaryQuantity = document.getElementById("detailQuantityDisplay_01");
 let summaryPrice = document.getElementById("itemPriceAmount_01");
 
-// let myElement = (document.createElement("p").innerText = "Dummy Text");
-
 //Gets localStorage info from modal
 let data = JSON.parse(localStorage.getItem("orderItem"));
-
-//Prints info from localStorage
-function printStorageInfo() {
-    let dataKeys = Object.keys(data);
-    let dataValues = Object.values(data);
-
-    for (i = 0; i < dataKeys.length; i++) {
-        summaryImage.setAttribute("src", dataValues[i].modalItemImage);
-        summaryName.innerText = dataValues[i].modalItemName;
-        summaryModel.innerText = dataValues[i].modalItemModel;
-        summaryQuantity.value = dataValues[i].modalItemQuantity;
-        summaryPrice.innerText = dataValues[i].modalItemUnitPrice;
-    }
-}
 
 window.addEventListener("load", () => {
     if (localStorage.length === 0 && data == null) {
         summaryItem.classList.add("hide");
         console.log("Condition 1");
-    } else if (localStorage.length === 1 && data.length == 1) {
-        summaryItem.classList.remove("hide");
-        printStorageInfo();
-        console.log("Condition 2");
-    } else if (localStorage.length === 1 && data.length >= 2) {
+    } else if (localStorage.length === 1 && data.length >= 1) {
+        let dataValues = Object.values(data);
         for (i = 0; i < data.length; i++) {
+            summaryImage.setAttribute("src", dataValues[i].modalItemImage);
+            summaryName.innerText = dataValues[i].modalItemName;
+            summaryModel.innerText = dataValues[i].modalItemModel;
+            summaryQuantity.value = dataValues[i].modalItemQuantity;
+            summaryPrice.innerText = dataValues[i].modalItemUnitPrice;
             summaryItem.classList.add("hide");
             let x = document.createElement("article");
+            x.innerHTML = summaryItem.innerHTML;
             x.classList.add("summaryItem");
-            printStorageInfo(x);
             itemList.append(x);
             console.log("fuck" + i);
             console.log(x);
         }
 
         console.log(data.length);
-        console.log("Condition 3");
+        console.log("Condition 2");
     }
 });
 
-// summaryItem.classList.remove("hide");
-//             let x = document.createElement("article");
-//             itemList.append(x);
-//             let y = summaryItem.innerHTML;
-//             x.innerHTML = y;
-//             printStorageInfo();
-//             x.classList.add("summaryItem");
-
-// let x = document.createElement("article");
-// let y = summaryItem.innerHTML;
-// for (i = 0; i < data.length; i++) {
-//     x.innerHTML = y;
-//     x.classList.add("summaryItem");
-//     itemList.append(x);
-//     printStorageInfo();
-// }
 window.addEventListener("storage", () => {
     location.reload();
+});
+
+window.addEventListener("unload", () => {
+    localStorage.clear();
 });
